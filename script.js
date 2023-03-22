24f71af0e16690cab0746ff7fd1e20b852ca409c
Gurneesh <singhgurneesh2022@gmail.com>
2023-03-22 10:00:36 +0530

    Animation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("Script running");
-
+// --------------------------------------HAMBERGER-------------------------------------------------------
 const menu = document.querySelector(".menu");
 const hamberger = document.querySelector(".hamberger");
 const menuIcon = document.querySelector(".menuIcon");
@@ -27,3 +26,32 @@ menuItems.forEach(function(menuItems)
 {
     menuItems.addEventListener("click",toggleMenu);
 })
+
+// ---------------------------------------------SCROLL----------------------------------------------------
+ScrollReveal(
+    { 
+        reset: true,
+        distance: '15px',
+        duration: 2000,
+        delay: 150
+      
+    });
+
+ScrollReveal().reveal('.Heading, .heading, #p1',{origin:'top'});
+ScrollReveal().reveal('.btech, .SSC, #p3, .left, #cl, #java,#css, #dsa',{origin:'left'});
+ScrollReveal().reveal('.HSC, #p2, .right, #cpp, #python, #html,#mysql',{origin:'right'});
+ScrollReveal().reveal('.ContentA, #p4, .content,.form',{origin:'bottom'});
+
+// ---------------------------------------------PROJECTS--------------------------------------------------
+let a=document.querySelector("#p1");
+let b=document.querySelector("#p2");
+let c=document.querySelector("#p3");
+let d=document.querySelector("#p4");
+a.addEventListener("click",alert1);
+b.addEventListener("click",alert1);
+c.addEventListener("click",alert1);
+d.addEventListener("click",alert1);
+function alert1()
+{
+    alert("Are you sure to leave the site");
+}
